201dfc9d0028b4971a9ab689852c84d315637872
Grant Rodgers <grantrodgers@google.com>
2020-04-08 13:32:10 -0700

    checkpoint: broker reconciler unit test skeleton

--- pkg/reconciler/broker/broker_test.go
+++ pkg/reconciler/broker/broker_test.go
@@ -15,3 +15,59 @@ limitations under the License.
 */
 
 package broker
+
+import (
+	"context"
+	"testing"
+
+	brokerv1beta1 "github.com/google/knative-gcp/pkg/apis/broker/v1beta1"
+	"github.com/google/knative-gcp/pkg/client/injection/ducks/duck/v1alpha1/resource"
+	brokerreconciler "github.com/google/knative-gcp/pkg/client/injection/reconciler/broker/v1beta1/broker"
+	gpubsub "github.com/google/knative-gcp/pkg/gclient/pubsub/testing"
+	"github.com/google/knative-gcp/pkg/reconciler"
+	. "github.com/google/knative-gcp/pkg/reconciler/testing"
+	"k8s.io/client-go/kubernetes/scheme"
+	"knative.dev/pkg/client/injection/ducks/duck/v1/addressable"
+	"knative.dev/pkg/configmap"
+	"knative.dev/pkg/controller"
+	logtesting "knative.dev/pkg/logging/testing"
+	. "knative.dev/pkg/reconciler/testing"
+)
+
+const (
+	testNS = "testnamespace"
+
+	testProject = "test-project-id"
+	generation  = 1
+)
+
+func init() {
+	// Add types to scheme
+	_ = brokerv1beta1.AddToScheme(scheme.Scheme)
+}
+
+func TestAllCases(t *testing.T) {
+	table := TableTest{{
+		Name: "bad workqueue key",
+		// Make sure Reconcile handles bad keys.
+		Key: "too/many/parts",
+	}, {
+		Name: "key not found",
+		// Make sure Reconcile handles good keys that don't exist.
+		Key: "foo/not-found",
+	}}
+
+	defer logtesting.ClearAll()
+	table.Test(t, MakeFactory(func(ctx context.Context, listers *Listers, cmw configmap.Watcher, testData map[string]interface{}) controller.Reconciler {
+		ctx = addressable.WithDuck(ctx)
+		ctx = resource.WithDuck(ctx)
+		r := &Reconciler{
+			Base:               reconciler.NewBase(ctx, controllerAgentName, cmw),
+			triggerLister:      listers.GetTriggerLister(),
+			configMapLister:    listers.GetConfigMapLister(),
+			CreateClientFn:     gpubsub.TestClientCreator(testData["ps"]),
+			targetsNeedsUpdate: make(chan struct{}),
+		}
+		return brokerreconciler.NewReconciler(ctx, r.Logger, r.RunClientSet, listers.GetBrokerLister(), r.Recorder, r, brokerv1beta1.BrokerClass)
+	}))
+}
