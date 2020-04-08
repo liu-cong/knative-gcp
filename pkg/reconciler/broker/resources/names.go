dde06c1a9585d20d33758c22031cd01196946db0
Grant Rodgers <grantrodgers@google.com>
2020-04-08 12:49:50 -0700

    Tests for topic/sub names

--- pkg/reconciler/broker/resources/names.go
+++ pkg/reconciler/broker/resources/names.go
@@ -19,28 +19,45 @@ package resources
 import (
 	"fmt"
 
-	"github.com/google/knative-gcp/pkg/apis/broker/v1beta1"
+	brokerv1beta1 "github.com/google/knative-gcp/pkg/apis/broker/v1beta1"
 )
+
+const (
+	pubsubMax       = 255
+	k8sNamespaceMax = 63
+	k8sNameMax      = 253
+	uidLength       = 36
+	md5Len          = 32
+)
+
+//TODO check length < 256 chars and truncate
+// pubsub resource max length: 255 chars
+// Namespace max length: 63 chars
+// broker name max length: 253 chars
+// trigger name max length: 253 chars
+// uid length: 36 chars
+// prefix + separators: 10 chars
+// 255 - 10 - 36 - 63 = 146 chars for name
 
 // GenerateDecouplingTopicName generates a deterministic topic name for a
 // Broker.
-func GenerateDecouplingTopicName(b *v1beta1.Broker) string {
-	return fmt.Sprintf("cre-b-%s-%s-%s", b.Namespace, b.Name, string(b.UID))
+func GenerateDecouplingTopicName(b *brokerv1beta1.Broker) string {
+	return fmt.Sprintf("cre-bkr_%s_%s_%s", b.Namespace, b.Name, string(b.UID))
 }
 
 // GenerateDecouplingSubscriptionName generates a deterministic subscription
 // name for a Broker.
-func GenerateDecouplingSubscriptionName(b *v1beta1.Broker) string {
-	return fmt.Sprintf("cre-b-%s-%s-%s", b.Namespace, b.Name, string(b.UID))
+func GenerateDecouplingSubscriptionName(b *brokerv1beta1.Broker) string {
+	return fmt.Sprintf("cre-bkr_%s_%s_%s", b.Namespace, b.Name, string(b.UID))
 }
 
 // GenerateRetryTopicName generates a deterministic topic name for a Trigger.
-func GenerateRetryTopicName(t *v1beta1.Trigger) string {
-	return fmt.Sprintf("cre-t-%s-%s-%s", t.Namespace, t.Name, string(t.UID))
+func GenerateRetryTopicName(t *brokerv1beta1.Trigger) string {
+	return fmt.Sprintf("cre-tgr_%s_%s_%s", t.Namespace, t.Name, string(t.UID))
 }
 
 // GenerateRetrySubscriptionName generates a deterministic subscription name
 // for a Trigger.
-func GenerateRetrySubscriptionName(t *v1beta1.Trigger) string {
-	return fmt.Sprintf("cre-t-%s-%s-%s", t.Namespace, t.Name, string(t.UID))
+func GenerateRetrySubscriptionName(t *brokerv1beta1.Trigger) string {
+	return fmt.Sprintf("cre-tgr_%s_%s_%s", t.Namespace, t.Name, string(t.UID))
 }
